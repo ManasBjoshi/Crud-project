a0f15176aaf43edccd79830dcc87d255c0ae5127
ManasBjoshi <149511859+ManasBjoshi@users.noreply.github.com>
2025-02-08 14:16:44 +0530

    Delete Amazonn directory

--- Amazonn/clothing/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from clothing import views
-urlpatterns =[
-    path("",views.home, name="home")
-]
--- Amazonn/clothing/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-def home(request):
-    prod = Product.objects.all()
-    return render(request,'clothing/home.html',{"prod":prod})
- 
